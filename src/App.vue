47a2b33c17aa65d9a36e208505d5aca901df28e2
Chrinovic MM <20mm364@esisalama.org>
2022-10-03 23:47:50 +0200

    Ajout du composant cardsComponent

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="app">
-
+    <restaurant-card/>
   </div>
 </template>
 
 <script>
 
+import restaurantCard from './components/CardsComponent.vue'
 
 export default {
   name: 'App',
+  components: {
+    restaurantCard
+  }
 }
 </script>
 
 <style>
+  @import url('./css/reset.css');
+  @import url("https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/CardsComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="restaurantCard">
+    <p>texte</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.restaurantCard{
+    width: 32%;
+    height: 30vh;
+}
+</style>
