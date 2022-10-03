3e8279abb83758d68cbfd7d29b461b5b64ba7c85
Chrinovic MM <20mm364@esisalama.org>
2022-10-04 00:50:28 +0200

    Style du component

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,5 @@ export default {
   @import url("https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap");
 #app {
   font-family: "Roboto", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/CardsComponent.vue
+++ src/components/CardsComponent.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="restaurantCard">
-    <p>texte</p>
+    <div class="cardimage">
+
+    </div>
+    <div class="cardinfo">
+      <div class="top">
+        <p class="name">Carte D'identite</p>
+        <p class="note">
+          <span>4.5</span>
+        </p>
+      </div>
+      <p class="time">40 - 60min</p>
+    </div>
   </div>
 </template>
 
@@ -18,6 +29,38 @@ export default {
    
     p{
       margin: 0px;
+    }
+
+    .cardimage{
+      background-image: url('img/imgcard.png');
+      background-size: cover;
+      background-position: center;
+      height: 70%;
+      width: 100%;
+    }
+
+    .cardinfo{
+     
+      .top{
+        margin: 10px 0px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .note{
+          background: #eeeeee;
+          height: 30px;
+          width: 30px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 0.9rem;
+          border-radius: 50%;
+        }
+      }
+
+      .time{
+        font-size: 0.9rem;
+      }
     }
 }
 </style>
