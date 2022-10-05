6ba5a9c2e8e655b4544ffea960442e2854b1e635
Chrinovic MM <20mm364@esisalama.org>
2022-10-05 11:45:37 +0200

    Ajout de this

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,11 +5,29 @@
 </template>
 
 <script>
+  //big data
+
+  import BDD from '../BDD.js'
+
+   //components
   import restaurentRow from '../components/RowComponent.vue'
 export default {
     name:"HomePage",
     components:{
       restaurentRow,
+    },
+    setup(){
+      class Restaurant{
+        constructor(name, note, image, drive_time){
+          this.name = name;
+          this.note = note;
+          this.image = image;
+          this.drive_time = drive_time;
+        }
+      }
+
+      var resto = new Restaurant('toto')
+      console.log(resto);
     }
 }
 </script>
